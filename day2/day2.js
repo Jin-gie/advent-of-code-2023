78bf0bfabcfcdbc89d2d31cd208576c6498f6182
Erin BERNARDONI <erin.bernardoni@outlook.fr>
2023-12-02 20:28:46 +0100

    [Day 2] Star 2 completed

--- day2/day2.js
+++ day2/day2.js
@@ -1,4 +1,5 @@
 // Star 1 answer: 2563
+// Star 2 answer : 70768
 
 const max_colors = {
   "red" : 12,
@@ -6,29 +7,56 @@ const max_colors = {
   "blue" : 14
 }
 
+const partOne = (input) => {
+  console.log(
+
+    input
+      .filter((game) => {
+        const hands = game.split(':')[1].split(';');
+
+        return(!hands.map((hand) => {
+          return(
+            hand.split(',').map((tirage) => {
+              const el = tirage.trim().split(' ')
+              // si valeur max >= valeur tirée => return true
+              // si valeur max < valeur tirée => return false
+              return (Number(max_colors[el[1]]) >= Number(el[0]))
+            }).includes(false)
+          )
+
+        }).includes(true))
+      })
+      .map((valid) => {return Number(valid.split(':')[0].trim().split(' ')[1])})
+      .reduce((a,b) =>{return a+b})
+
+  )
+}
+
+const partTwo = (input) => {
+  const colors = ['red', 'green', 'blue'];
+  
+  console.log(
+  input.map((game) => {
+    tirages = game.split(':')[1].split(/,|;/);
+
+    return (
+      colors.map((color) => {
+        const color_tirages = tirages
+          .filter((tir) => {
+            return tir.includes(color);
+          })
+          .map((el) => {return (Number(el.trim().split(' ')[0]))})
+          
+        return Math.max(...color_tirages)
+      }).reduce((a,b) => {return a*b})
+    )
+    
+  }).reduce((a,b) => { return a+b})
+  )
+}
+
+
 readInputByLines("/day2/input.txt")
   .then((input) => {
-    console.log(
-
-      input
-        .filter((game) => {
-          const hands = game.split(':')[1].split(';');
-
-          return(!hands.map((hand) => {
-            return(
-              hand.split(',').map((tirage) => {
-                const el = tirage.trim().split(' ')
-                // si valeur max >= valeur tirée => return true
-                // si valeur max < valeur tirée => return false
-                return (Number(max_colors[el[1]]) >= Number(el[0]))
-              }).includes(false)
-            )
-
-          }).includes(true))
-        })
-        .map((valid) => {return Number(valid.split(':')[0].trim().split(' ')[1])})
-        .reduce((a,b) =>{return a+b})
-
-    )
-
+    partTwo(input)
   })
